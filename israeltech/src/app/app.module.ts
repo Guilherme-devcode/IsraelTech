a9a9e759a1759b866f635ffe5b434f9f65b7f60c
Guilherme Mesquita Rocha <guilherme.mesquita.rocha@gmail.com>
2022-02-09 10:52:15 -0300

    adicionando sessão produtos e criação de produtos

--- israeltech/src/app/app.module.ts
+++ israeltech/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
+import { HomeComponent } from './views/home/home.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { ProductsComponent } from './views/products/products.component';
+import { ForDirective } from './directives/for.directive';
+import { ProductsCreateComponent } from './components/product/products-create/products-create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    ProductsComponent,
+    ForDirective,
+    ProductsCreateComponent
   ],
   imports: [
     BrowserModule,
